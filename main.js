0d5438fd75c0dcc9e5a0cc52445c5f0e49151851
Tho Vo <votuongtho@gmail.com>
2024-11-17 19:13:50 +0100

    feat: complete optimize

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 import './style.css';
-import { setupCarousel } from './js/carousel';
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    modules: [Navigation, Pagination],
 
-setupCarousel(document.querySelector('#simpleCarousel'));
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+// Now you can use all slider methods like
+swiper.slideNext();
